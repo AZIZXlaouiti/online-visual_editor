f0c0c21bb3b2559d58d030b21624cc7fbdd4eb40
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-11 18:49:47 -0500

    making the editor more functional

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+const app = require("express")
+const http = require("http").Server(app)
+const io = require("socket.io")(http , {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  })
+io.on('connection', async(socket) => {
+ 
+    socket.on('new-operations', (msg) => {
+    socket.broadcast.emit('new-remote-operations', msg);
+    });
+});
+
+// listen for socket connection
+http.listen(4000 , async()=>{
+    console.log("listening on *:4000")
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBlockquotePlugin, createBoldPlugin, createCodeBlockPlugin, createCodePlugin, createHeadingPlugin, createHighlightPlugin, createItalicPlugin, createParagraphPlugin, createReactPlugin, createStrikethroughPlugin, createUnderlinePlugin, Plate } from '@udecode/plate'
+import { createPlateOptions } from '@udecode/plate';
+import { createPlateComponents } from '@udecode/plate';
+import React, { useMemo, useState , useRef, useEffect} from 'react'
+import { createEditor, BaseEditor, Descendant, Operation } from 'slate'
+import { Slate, Editable, withReact, ReactEditor } from 'slate-react'
+import { HistoryEditor } from 'slate-history'
+import io from 'socket.io-client'
+import { ELEMENT_H1, ELEMENT_PARAGRAPH, MARK_STRIKETHROUGH, MARK_UNDERLINE, MARK_CODE, ELEMENT_H2, MARK_BOLD, MARK_ITALIC, ELEMENT_BLOCKQUOTE } from '@udecode/plate'
+const socket = io('http://localhost:4000')
+const editableProps = {
+  placeholder: 'Type…',
+  style: {
+    padding: '15px',
+  },
+};
+const createElement = (
+  text = '',
+  {
+    type = ELEMENT_PARAGRAPH,
+    mark,
+  }: {
+    type?: string;
+    mark?: string;
+  } = {}
+) => {
+  const leaf = { text };
+  // if (mark) {
+  //   leaf[mark] = true;
+  // }
 
-function App() {
+  return {
+    type,
+    children: [leaf],
+  };
+};
+const components = createPlateComponents()
+const options = createPlateOptions()
+const basicNodesPlugins = [
+  // editor
+  createReactPlugin(),
+  createHighlightPlugin(),
+  // elements
+  createParagraphPlugin(),      // paragraph element
+  createBlockquotePlugin(),     // blockquote element
+  createCodeBlockPlugin(),      // code block element
+  createHeadingPlugin(),        // heading elements
+  // marks
+  createBoldPlugin(),           // bold mark
+  createItalicPlugin(),         // italic mark
+  createUnderlinePlugin(),      // underline mark
+  createStrikethroughPlugin(),  // strikethrough mark
+  createCodePlugin()            // code mark
+]
+const basicNodesInitialValue = [
+  createElement('🧱 Elements', { type: ELEMENT_H1 }),
+  createElement('🔥 Basic Elements', { type: ELEMENT_H2 }),
+  createElement('Blockquote', { type: ELEMENT_BLOCKQUOTE }),
+  createElement('This text is bold.', { mark: MARK_BOLD }),
+  createElement('This text is italic.', { mark: MARK_ITALIC }),
+  createElement('This text is underlined.', {
+    mark: MARK_UNDERLINE,
+  }),
+  {
+    type: ELEMENT_PARAGRAPH,
+    children: [
+      {
+        text: 'This text is bold, italic and underlined.',
+        [MARK_BOLD]: true,
+        [MARK_ITALIC]: true,
+        [MARK_UNDERLINE]: true,
+      },
+    ],
+  },
+  createElement('This is a strikethrough text.', {
+    mark: MARK_STRIKETHROUGH,
+  }),
+  createElement('This is an inline code.', { mark: MARK_CODE }),
+]
+const  App =() =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="editor container"
+    >
+
+      <Plate
+        id="1"
+        onChange={value => console.log(value)}
+        initialValue={basicNodesInitialValue}
+        editableProps={editableProps}
+        components={components}
+        options={options}
+        plugins={basicNodesPlugins}
+      />
     </div>
   );
 }
